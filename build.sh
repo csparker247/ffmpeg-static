17f6e5b122b2858df3a24b9e9ab002800df01c54
csparker247 <csparker247@gmail.com>
2012-10-24 00:38:02 -0400

    Updates for lavf-enabled x264
    OSX tailored modifications. Installs libav before x264 so that x264
    builds with lavf support. Builds well on OSX 10.6 for x86_64 10.6+

--- build.sh
+++ build.sh
@@ -16,13 +16,14 @@ mkdir -p "$BUILD_DIR" "$TARGET_DIR"
 echo "#### FFmpeg static build, by STVS SA ####"
 cd $BUILD_DIR
 ../fetchurl "http://www.tortall.net/projects/yasm/releases/yasm-1.2.0.tar.gz"
+../fetchurl "http://libav.org/releases/libav-0.8.3.tar.gz"
 ../fetchurl "http://zlib.net/zlib-1.2.7.tar.bz2"
 ../fetchurl "http://www.bzip.org/1.0.6/bzip2-1.0.6.tar.gz"
 ../fetchurl "ftp://ftp.simplesystems.org/pub/libpng/png/src/libpng-1.2.50.tar.gz"
 ../fetchurl "http://downloads.xiph.org/releases/ogg/libogg-1.3.0.tar.gz"
 ../fetchurl "http://downloads.xiph.org/releases/vorbis/libvorbis-1.3.3.tar.gz"
 ../fetchurl "http://downloads.xiph.org/releases/theora/libtheora-1.1.1.tar.bz2"
-../fetchurl "http://webm.googlecode.com/files/libvpx-v1.0.0.tar.bz2"
+# ../fetchurl "http://webm.googlecode.com/files/libvpx-v1.0.0.tar.bz2"
 ../fetchurl "http://downloads.sourceforge.net/project/faac/faac-src/faac-1.28/faac-1.28.tar.bz2?use_mirror=auto"
 ../fetchurl "ftp://ftp.videolan.org/pub/videolan/x264/snapshots/x264-snapshot-20120425-2245.tar.bz2"
 ../fetchurl "http://downloads.xvid.org/downloads/xvidcore-1.3.2.tar.gz"
@@ -66,10 +67,10 @@ cd "$BUILD_DIR/libtheora-1.1.1"
 ./configure --prefix=$TARGET_DIR --enable-static --disable-shared
 make -j 4 && make install
 
-echo "*** Building livpx ***"
-cd "$BUILD_DIR/libvpx-v1.0.0"
-./configure --prefix=$TARGET_DIR --disable-shared
-make -j 4 && make install
+#echo "*** Building livpx ***"
+#cd "$BUILD_DIR/libvpx-v1.0.0"
+#./configure --prefix=$TARGET_DIR --disable-shared
+#make -j 4 && make install
 
 echo "*** Building faac ***"
 cd "$BUILD_DIR/faac-1.28"
@@ -79,11 +80,14 @@ cd "$BUILD_DIR/faac-1.28"
 sed -i -e "s|^char \*strcasestr.*|//\0|" common/mp4v2/mpeg4ip.h
 make -j 4 && make install
 
-echo "*** Building x264 ***"
-cd "$BUILD_DIR/x264-snapshot-20120425-2245"
-./configure --prefix=$TARGET_DIR --enable-static --disable-shared
+cd "$BUILD_DIR/libav-0.8.3"
+./configure --prefix=$TARGET_DIR/lavf --enable-gpl --disable-debug --enable-runtime-cpudetect
 make -j 4 && make install
 
+echo "*** Building x264 ***"
+cd "$BUILD_DIR/x264-snapshot-20120425-2245"
+CFLAGS="-I$TARGET_DIR/lavf/include" LDFLAGS="-L$TARGET_DIR/lavf/lib -framework CoreFoundation -framework CoreVideo -framework VideoDecodeAcceleration" ./configure --prefix=$TARGET_DIR --host=x86_64-apple-darwin --enable-static
+make -j 4 && make install
 
 echo "*** Building xvidcore ***"
 cd "$BUILD_DIR/xvidcore/build/generic"
@@ -97,13 +101,12 @@ cd "$BUILD_DIR/lame-3.99.5"
 make -j 4 && make install
 
 # FIXME: only OS-sepcific
-rm -f "$TARGET_DIR/lib/*.dylib"
-rm -f "$TARGET_DIR/lib/*.so"
+#rm -f "$TARGET_DIR/lib/*.dylib"
+#rm -f "$TARGET_DIR/lib/*.so"
 
 # FFMpeg
 echo "*** Building FFmpeg ***"
 cd "$BUILD_DIR/ffmpeg-0.10.2"
 patch -p1 <../../ffmpeg_config.patch
-CFLAGS="-I$TARGET_DIR/include" LDFLAGS="-L$TARGET_DIR/lib -lm" ./configure --prefix=${OUTPUT_DIR:-$TARGET_DIR} --extra-version=static --disable-debug --disable-shared --enable-static --extra-cflags=--static --disable-ffplay --disable-ffserver --disable-doc --enable-gpl --enable-pthreads --enable-postproc --enable-gray --enable-runtime-cpudetect --enable-libfaac --enable-libmp3lame --enable-libtheora --enable-libvorbis --enable-libx264 --enable-libxvid --enable-bzlib --enable-zlib --enable-nonfree --enable-version3 --enable-libvpx --disable-devices
+CFLAGS="-I$TARGET_DIR/include" LDFLAGS="-L$TARGET_DIR/lib -lm" ./configure --cc=clang --prefix=${OUTPUT_DIR:-$TARGET_DIR} --extra-version=static --disable-debug --disable-shared --enable-static --extra-cflags=--static --disable-ffplay --disable-ffserver --disable-doc --enable-gpl --enable-pthreads --enable-postproc --enable-gray --enable-runtime-cpudetect --enable-libfaac --enable-libmp3lame --enable-libtheora --enable-libvorbis --enable-libx264 --enable-libxvid --enable-bzlib --enable-zlib --enable-nonfree --enable-version3 --disable-devices
 make -j 4 && make install
-
