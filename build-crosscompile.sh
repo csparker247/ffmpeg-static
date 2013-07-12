0e1ce06e19b67fcc85412aae7bfba35b99c6370b
Seth Parker <csparker247@gmail.com>
2013-07-12 10:58:57 -0400

    More bad attempts

--- build-crosscompile.sh
+++ build-crosscompile.sh
@@ -49,29 +49,29 @@ make install PREFIX=$TARGET_DIR
 
 echo "*** Building libpng ***"
 cd "$BUILD_DIR/libpng-1.6.2"
-./configure --prefix=$TARGET_DIR --host=x86_64-apple-darwin11 --with-sysroot=/SDKs/MacOSX.platform/MacOSX10.6.sdk --enable-static --disable-shared
+./configure --prefix=$TARGET_DIR --host=x86_64-apple-darwin10 --with-sysroot=/SDKs/MacOSX.platform/MacOSX10.6.sdk --enable-static --disable-shared
 make -j 4 && make install
 
 # Ogg before vorbis
 echo "*** Building libogg ***"
 cd "$BUILD_DIR/libogg-1.3.0"
-./configure --prefix=$TARGET_DIR --host=x86_64-apple-darwin11 --with-sysroot=/SDKs/MacOSX.platform/MacOSX10.6.sdk --enable-static --disable-shared
+./configure --prefix=$TARGET_DIR --host=x86_64-apple-darwin10 --with-sysroot=/SDKs/MacOSX.platform/MacOSX10.6.sdk --enable-static --disable-shared
 make -j 4 && make install
 
 # Vorbis before theora
 echo "*** Building libvorbis ***"
 cd "$BUILD_DIR/libvorbis-1.3.3"
-./configure --prefix=$TARGET_DIR --target=x86_64-darwin11 --host=x86_64-apple-darwin11 --enable-static --disable-shared
+./configure --prefix=$TARGET_DIR --target=x86_64-darwin10 --host=x86_64-apple-darwin10 --enable-static --disable-shared
 make -j 4 && make install
 
 echo "*** Building libtheora ***"
 cd "$BUILD_DIR/libtheora-1.1.1"
-./configure --prefix=$TARGET_DIR --target=x86_64-darwin11 --host=x86_64-apple-darwin11 --enable-static --disable-shared
+./configure --prefix=$TARGET_DIR --target=x86_64-darwin10 --host=x86_64-apple-darwin10 --enable-static --disable-shared
 make -j 4 && make install
 
 echo "*** Building livpx ***"
 cd "$BUILD_DIR/libvpx-v1.1.0"
-./configure --prefix=$TARGET_DIR --target=x86_64-darwin11-gcc --sdk-path=/SDKs/MacOSX.platform/MacOSX10.6.sdk --enable-runtime-cpu-detect --enable-static --disable-shared
+./configure --prefix=$TARGET_DIR --target=x86_64-darwin10-gcc --sdk-path=/SDKs/MacOSX.platform/MacOSX10.6.sdk --enable-runtime-cpu-detect --enable-static --disable-shared
 make -j 4 && make install
 
 # echo "*** Building faac ***"
@@ -83,7 +83,7 @@ make -j 4 && make install
 
 echo "*** Building fdk-aac ***"
 cd "$BUILD_DIR/fdk-aac-0.1.0"
-./configure --prefix=$TARGET_DIR --host=x86_64-apple-darwin11 --with-sysroot=/SDKs/MacOSX.platform/MacOSX10.6.sdk --enable-static --disable-shared
+./configure --prefix=$TARGET_DIR --host=x86_64-apple-darwin10 --with-sysroot=/SDKs/MacOSX.platform/MacOSX10.6.sdk --enable-static --disable-shared
 make -j 4 && make install
 
 cd "$BUILD_DIR/libav-9.8"
@@ -92,18 +92,18 @@ make -j 4 && make install
 
 echo "*** Building x264 ***"
 cd "$BUILD_DIR/x264-snapshot-20130501-2245"
-CFLAGS="-I$TARGET_DIR/lavf/include" LDFLAGS="-L$TARGET_DIR/lavf/lib -framework CoreFoundation -framework CoreVideo -framework VideoDecodeAcceleration" ./configure --prefix=$TARGET_DIR --host=x86_64-apple-darwin11 --enable-static --disable-asm --sysroot=/SDKs/MacOSX.platform/MacOSX10.6.sdk
+CFLAGS="-I$TARGET_DIR/lavf/include -mmacosx-version-min=10.6" LDFLAGS="-L$TARGET_DIR/lavf/lib -framework CoreFoundation -framework CoreVideo -framework VideoDecodeAcceleration" ./configure --prefix=$TARGET_DIR --host=x86_64-apple-darwin10 --enable-static --disable-asm --sysroot=/SDKs/MacOSX.platform/MacOSX10.6.sdk
 make -j 4 && make install
 
 echo "*** Building xvidcore ***"
 cd "$BUILD_DIR/xvidcore/build/generic"
-./configure --prefix=$TARGET_DIR --host=x86_64-apple-darwin11 --enable-static --disable-shared
+./configure --prefix=$TARGET_DIR --host=x86_64-apple-darwin10 --enable-static --disable-shared
 make -j 4 && make install
 #rm $TARGET_DIR/lib/libxvidcore.so.*
 
 echo "*** Building lame ***"
 cd "$BUILD_DIR/lame-3.99.5"
-./configure --prefix=$TARGET_DIR --host=x86_64-apple-darwin11 --enable-static --disable-shared
+./configure --prefix=$TARGET_DIR --host=x86_64-apple-darwin10 --enable-static --disable-shared
 make -j 4 && make install
 
 # FIXME: only OS-sepcific
@@ -114,5 +114,5 @@ make -j 4 && make install
 echo "*** Building FFmpeg ***"
 cd "$BUILD_DIR/ffmpeg-2.0"
 # --enable-libvpx 
-CFLAGS="-I$TARGET_DIR/include" LDFLAGS="-L$TARGET_DIR/lib -lm" ./configure --arch=x86_64 --target-os=darwin --sysroot=/SDKs/MacOSX.platform/MacOSX10.6.sdk --enable-cross-compile --cc=clang --prefix=${OUTPUT_DIR:-$TARGET_DIR} --extra-version=static --disable-debug --disable-shared --enable-static --extra-cflags=--static --disable-ffplay --disable-ffserver --disable-doc --enable-gpl --enable-pthreads --enable-postproc --enable-gray --enable-runtime-cpudetect --enable-libfdk-aac --enable-libmp3lame --enable-libtheora --enable-libvorbis --enable-libx264 --enable-libxvid --enable-bzlib --enable-zlib --enable-nonfree --enable-version3 --disable-devices
+CFLAGS="-I$TARGET_DIR/include -mmacosx-version-min=10.6" LDFLAGS="-L$TARGET_DIR/lib -lm" ./configure --arch=x86_64 --target-os=darwin --sysroot=/SDKs/MacOSX.platform/MacOSX10.6.sdk --enable-cross-compile --cc=clang --prefix=${OUTPUT_DIR:-$TARGET_DIR} --extra-version=static --disable-debug --disable-shared --enable-static --extra-cflags=--static --disable-ffplay --disable-ffserver --disable-doc --enable-gpl --enable-pthreads --enable-postproc --enable-gray --enable-runtime-cpudetect --enable-libfdk-aac --enable-libmp3lame --enable-libtheora --enable-libvorbis --enable-libx264 --enable-libxvid --enable-bzlib --enable-zlib --enable-nonfree --enable-version3 --disable-devices
 make -j 4 && make install
